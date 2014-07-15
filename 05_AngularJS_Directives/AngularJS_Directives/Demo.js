1cdf589deb01ed022115b0cae235168a0a85bd9a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-15 14:34:30 +0100

    05_AngularJS Directives

---
+++ 05_AngularJS_Directives/AngularJS_Directives/Demo.js
@@ -0,0 +1,29 @@
+﻿(function (angular) {
+    var module = angular.module("myApp", []);
+
+    module.directive("myClick", function ($http) {
+        return {
+            restrict: "EA",
+            link: function (scope, element) {
+                element.bind("click", function () {
+                    alert("I was clicked");
+                });
+            }
+        };
+    });
+
+
+    module.directive("myTemplate", function () {
+        return {
+            restrict: "E",
+            replace: true,
+            transclude: true,
+            //scope: true,
+            scope: {
+                myDemo: "="
+            },
+            templateUrl: "/myTemplate.html"
+        }
+    })
+
+}(angular))
