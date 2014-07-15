cce46c213a182fd25496a512d971b5b57b9946d5
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-15 11:09:39 +0100

    04_AngularJS_Introduction

---
+++ 04_AngularJS_Introduction/AngularJS_Introduction/Demo.js
@@ -0,0 +1,32 @@
+﻿(function (angular) {
+
+    var demoApp = angular.module("demoApp", []);
+
+    demoApp.service("utils", function() {
+        return {
+            log:function (msg) { console.log(msg); }
+
+        };
+    });
+
+    demoApp.controller("DemoCtrl", ["$scope", "$http", "utils", function($scope, $hello, utils) {
+        utils.log("In DemoCtrl");
+
+        $scope.model = {
+            name: "Mike"
+        };
+
+        $scope.people = [
+            { name: "Maurice" },
+            { name: "John" },
+            { name: "Jack" }
+        ];
+
+        $scope.clicked = function() {
+            console.log($scope.model);
+        }
+    }]);
+
+
+}(angular));
+
