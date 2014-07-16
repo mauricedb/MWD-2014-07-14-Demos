6f6b6ac9adabe828137a39fbb64731a6e37253d0
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-16 10:32:28 +0100

    07_Single Page Applications

---
+++ 07_Single Page Applications/Single Page Applications/Demo.js
@@ -0,0 +1,45 @@
+﻿(function() {
+    var app = angular.module("myApp", ["ngRoute"]);
+
+
+    app.config(function($routeProvider) {
+
+        $routeProvider.when("/list", {
+            controller: "ListCtrl",
+            templateUrl: "/ListTemplate.html"
+        });
+
+        $routeProvider.when("/details/:id", {
+            controller: "DetailsCtrl",
+            templateUrl: "/DetailsTemplate.html",
+            resolve: {
+                id: function($route) {
+                    return $route.current.params.id;
+                }
+                //book: function($http, $route) {
+                //    return $http.get("/api/books/"+ $route.current.params.id)
+                //}
+            }
+        });
+
+
+        $routeProvider.otherwise({ redirectTo: "/list" });
+
+    });
+
+    app.controller("ListCtrl", function ($scope, $location) {
+        console.log("In ListCtrl");
+        
+
+        $scope.goto = function(id) {
+            $location.path("/details/" + id);
+        }
+    });
+
+    app.controller("DetailsCtrl", function ($scope, id) {
+        $scope.id = id;
+        console.log("In DetailsCtrl");
+
+    });
+
+}());
