16395886d8548e73a49b2e1e61aee67be96a26e5
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-17 11:41:46 +0100

    10_HTML5_Networking

---
+++ 10_HTML5_Networking/ServerSentEvents/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerSentEvents.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            Response.ContentType = "text/event-stream";
+
+            for (int i = 0; i < 10; i++)
+            {
+                Response.Write("data: " + DateTime.Now.ToLongTimeString() + "\n\n");
+                Response.Flush();
+                Thread.Sleep(1000);
+            }
+
+            return Content("data: " + DateTime.Now.ToLongTimeString() + "\n\n");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 10_HTML5_Networking/ServerSentEvents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServerSentEvents.Startup))]
+namespace ServerSentEvents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
