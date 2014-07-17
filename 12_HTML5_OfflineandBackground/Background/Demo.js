94c80e641a069d606116d4dccd32cd5af9a816bb
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-17 16:15:56 +0100

    12_HTML5_Offline and Background

---
+++ 12_HTML5_OfflineandBackground/Background/Demo.js
@@ -0,0 +1,23 @@
+﻿$(function() {
+
+    $("#btnStart").click(function() {
+
+        //for (var prime = 1; prime < 50000; prime++) {
+        //    var isPrime = true;
+        //    for (var i = 2; i < prime - 1; i++) {
+        //        if (prime % i === 0) {
+        //            isPrime = false;
+        //        }
+        //    }
+        //    if (isPrime) {
+        //        $("<li>").text(prime).prependTo("#primes");
+        //    }
+        //}
+
+        var worker = new Worker("primes.js");
+        worker.onmessage = function(e) {
+            $("<li>").text(e.data).prependTo("#primes");
+        }
+    });
+
+});
---
+++ 12_HTML5_OfflineandBackground/Background/primes.js
@@ -0,0 +1,12 @@
+﻿for (var prime = 1; prime < 100000; prime++) {
+    var isPrime = true;
+    for (var i = 2; i < prime - 1; i++) {
+        if (prime % i === 0) {
+            isPrime = false;
+        }
+    }
+    if (isPrime) {
+        //$("<li>").text(prime).prependTo("#primes");
+        postMessage(prime);
+    }
+}
