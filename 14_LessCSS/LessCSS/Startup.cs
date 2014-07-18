a855942dbf443820cfbc59a1d1b4450341a1fdcb
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-18 15:56:59 +0100

    14_LessCSS

---
+++ 14_LessCSS/LessCSS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LessCSS.Startup))]
+namespace LessCSS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
